7805f2027949793b6522d9c8a15ae56dc904569d
Yassin Aberkan <yaberkan.pro@gmail.com>
2021-09-09 14:11:38 +0200

    google ver

--- components/google-maps.js
+++ components/google-maps.js
@@ -27,11 +27,14 @@ const mapElementStyle = { height: '100%' };
 
 export default function GoogleMaps(){
   return (
+    <>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB01u6Zgb0r_X647UpE4P2W0yAjQeQO_Xo&callback=initMap"></script>
     <RegularMap
       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyB01u6Zgb0r_X647UpE4P2W0yAjQeQO_Xo"
       loadingElement={<div style={ loadingElementStyle } />}
       containerElement={<div className="h-48 md:h-full" />}
       mapElement={<div style={ mapElementStyle } />}
-    />
+    />  
+    </>
   );
 } 
